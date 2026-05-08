e4770d26e33d450aa618e10383e94ebb6cf0300b
ebbestad <ebbestad@hf-linbast2.no.habbfarm.net>
2026-05-08 18:14:47 +0200

    update to foreman vars

--- scripts/galaxy_install.sh
+++ scripts/galaxy_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ansible-galaxy role install --role-file roles/requirements.yml --force
-ansible-galaxy collection install --requirements-file collections/requirements.yml
+ansible-galaxy collection install --requirements-file collections/requirements.yml --force
